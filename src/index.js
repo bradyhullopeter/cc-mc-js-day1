006e3a7a40912039f0afef2c218798f2708d2e55
Brady Hullopeter <hullopeter.brady@mayo.edu>
2017-11-28 12:38:37 -0600

    add test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 (() => {
   console.log('Hello, World!')
 })()
+
+export function getSalutation(name) {
+  return `Hello, ${name}`;
+}
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,9 +1,15 @@
-import index from './index';
+import {getSalutation} from './index';
 import {expect} from 'chai';
 
 describe('index.js', () => {
 
   it('should run this test', () => {
     expect(true).to.equal(true)
+  })
+
+  it('should return proper salutation', () => {
+    const name = 'Collin'
+    const salutation = getSalutation(name)
+    expect(salutation).to.equal(`Hello, ${name}`)
   })
 })
