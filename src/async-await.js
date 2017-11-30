ba81cc7bc389543bd2537cca6c1aa32a065fa8fe
Bradley DeJong <DeJong.Bradley@mayo.edu>
2017-11-30 09:49:21 -0600

    async/await solution

--- src/async-await.js
+++ src/async-await.js
@@ -2,7 +2,17 @@ import loadFile from './file-loader-p.js';
 import logContent from './log-content.js';
 
 async function main() {
-  //TODO: use async/await to load/log files in order
+  const p1 = loadFile('file1');
+  const p2 = loadFile('file2');
+  const p3 = loadFile('file3');
+  const p4 = loadFile('file4');
+  const p5 = loadFile('file5');
+
+  logContent('file1', await p1);
+  logContent('file2', await p2);
+  logContent('file3', await p3);
+  logContent('file4', await p4);
+  logContent('file5', await p5);
 }
 
 main();
