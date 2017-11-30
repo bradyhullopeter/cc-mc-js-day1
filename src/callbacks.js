48d4207d12fdb9520f50b50607236d61ec7c0bd2
Bradley DeJong <DeJong.Bradley@mayo.edu>
2017-11-30 09:44:43 -0600

    callbacks solution

--- src/callbacks.js
+++ src/callbacks.js
@@ -2,9 +2,22 @@ import loadFile from './file-loader';
 import logContent from './log-content';
 
 const results = {};
+const fileNames = ['file1', 'file2', 'file3', 'file4', 'file5'];
+
 function logFileData(fileName, data) {
-  //TODO: keep track of responses to serialize them in order
-  logContent(fileName, data);
+  results[fileName] = data;
+  
+  for (let i = 0; i < fileNames.length; i++) {
+    if (fileNames[i] in results) {
+      if (results[fileNames[i]]) {
+        logContent(fileNames[i], results[fileNames[i]]);
+        results[fileNames[i]] = false;
+      }
+    }
+    else {
+      return;
+    }
+  }
 }
 
 loadFile('file1', data => logFileData('file1', data));
